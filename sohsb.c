1c5a02b21de68fb8d2d7b0e677dd39410f5ff1e6
alwadey711 <osamaalwadey2022@gmail.com>
2026-09-04 03:46:01 +0300

    update 2026-09-04 03:46

--- sohsb.c
+++ sohsb.c
@@ -1,10 +1,11 @@
 #include "osama.h"
 int main(int argc, char *argv[]){
-	if(argc != 2){
-		printf("usage : sohsb username");
+	if(argc != 3){
+		printf("usage : sohsb username password");
 	}else{
 		FILE *file = fopen("users.txt","r");
 		FILE *filew = fopen("users.txt","a");
+		FILE *passfile = fopen("passwords.txt","a");
 		int last_uid = -1;
 		char line[256];
 		while (fgets(line, sizeof(line), file) != NULL) {
@@ -26,7 +27,9 @@ int main(int argc, char *argv[]){
 	        printf("Error opening the file!\n");
 		    return 1;
 	    }else{
-	    	fprintf(filew,"%s:%d\n",argv[1],++last_uid);
+	    	int li = ++last_uid;
+	    	fprintf(filew,"%s:%d\n",argv[1],li);
+	    	fprintf(passfile,"%d:%s",li,argv[2]);
 	    }
 	}
 }
